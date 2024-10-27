ad79491a2c7ce74450470682d0914deb152175ba
asadanik <s.engineer63@gmail.com>
2024-10-27 15:32:29 +0600

    enviroment variable updated for production application

--- shared/const.ts
+++ shared/const.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   (process.env.NODE_ENV || process.env.NEXT_PUBLIC_NODE_ENV) === 'development'
     ? `http://localhost:${process.env.PORT ?? process.env.NEXT_PUBLIC_PORT}/api`
-    : 'http://dev.suno.com/api';
+    : 'https://share-link-plum.vercel.app/api';
 
 export const X_NEXT_TOKEN = 'x-next-token';
 
